2db560f0a735fa9f6457835c4854a9513e3688e6
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-07 11:11:38 +0200

    skrócenie kodu do rozwijania menu

--- js.js
+++ js.js
@@ -1,25 +1,19 @@
 const btnShow = document.querySelector('.fa-bars');
-const btnHide = document.querySelector('.hide');
+const btnHide = document.querySelector('.fa-times');
 const menuBurger = document.querySelector('.burger');
 const ulShow = document.querySelector('ul');
 
-showMenu = () =>{
-    menuBurger.classList.add('active')
-    ulShow.classList.add('active')
+const showHideMenu = () =>{
+    ulShow.classList.toggle('active');
+    menuBurger.classList.toggle('active');
+    btnShow.classList.toggle('hide');
+    btnHide.classList.toggle('hide');
 }
-hideMenu = ()=>{
-    menuBurger.classList.remove('active');
-    ulShow.classList.remove('active');
-}
-// const menu = () =>{
-//     if(ulShow.hasAttribute('.active')){
-//         hideMenu()
-//     }else{
-//         showMenu()
-//     }
-// }
 
 
 
-btnShow.addEventListener('click', showMenu);
-btnHide.addEventListener('click', hideMenu);
+
+
+
+
+menuBurger.addEventListener('click', showHideMenu);
